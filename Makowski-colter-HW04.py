0d43bd31629e193cc9be896139a9b80e5e3d09e9
cmakowsk <cmakowsk@uwyo.edu>
2024-11-11 05:24:12 +0000

    tried something

--- Makowski-colter-HW04.py
+++ Makowski-colter-HW04.py
@@ -7,3 +7,23 @@
 # your
 # comments
 # here
+
+# use this to split from colon split_str = Calc_str.split(operator)
+from pathlib import Path
+path = Path('prompt.txt')
+contents = path.read_text()
+lines = contents.splitlines()
+with open("out.txt", "w") as out_file:
+    for line in lines:
+        code_string = line.strip()
+        list_of_amounts = code_string.split(" ")
+        space_amount = 0
+        star_amount = 0
+        for amount in list_of_amounts:
+            if amount.startswith("w:"):
+                space_amount = int(amount[2:])
+            if amount.startswith("*:"):
+                star_amount = int(amount[2:])
+        output_line = (space_amount * " ") + (star_amount * "*") + "\n"
+        out_file.write(output_line)
+
